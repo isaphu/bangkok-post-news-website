abf70cb11ee3ad8d9c09c81612e216ac8e638fe8
Isa Phuyuthanon <uronohan@gmail.com>
2022-03-23 23:20:01 +0700

    update

--- js/retrieve-news.js
+++ js/retrieve-news.js
@@ -1,10 +1,13 @@
+// select for class in DOM
 const searchForm = document.querySelector(".search");
 const input = document.querySelector(".input");
 const newsList = document.querySelector(".news-list");
 
+//adding event lister to the button called retrieveNews
 searchForm.addEventListener("submit", retrieveNews);
 
 function retrieveNews(e) {
+  //check if user input the submit, if not alert the following
   if (input.value == "") {
     alert("Input field is empty!");
     return;
@@ -13,11 +16,19 @@ function retrieveNews(e) {
 
   e.preventDefault();
 
+  /* Obtain the url from the NEWSapi: https://newsapi.org/docs/endpoints/everything 
+  input api key obtained from the service. The variable called topic will take whatever
+  the user input and insert it into the URL.
+  */
   const token = config.apiKey;
-  let topic = input.value;
+  const topic = input.value;
 
-  let url = `https://newsapi.org/v2/everything?q=${topic}&apiKey=${token}`;
+  const url = `https://newsapi.org/v2/everything?q=${topic}&apiKey=${token}`;
 
+  /* fetch the data from the above URL, for each article there will be a new list element 
+  created. It will set the attribute as follow, the URL and when user press the link it will
+  open a new tab. It will also get title and append it to the list and lastly append to the newsList
+  */
   fetch(url)
     .then((res) => {
       return res.json();
@@ -25,8 +36,8 @@ function retrieveNews(e) {
     .then((data) => {
       console.log(data);
       data.articles.forEach((articles) => {
-        let li = document.createElement("li");
-        let a = document.createElement("a");
+        const li = document.createElement("li");
+        const a = document.createElement("a");
         a.setAttribute("href", articles.url);
         a.setAttribute("target", "_blank");
         a.textContent = articles.title;
--- js/toggle-menu.js
+++ js/toggle-menu.js
@@ -1,6 +1,11 @@
+// on the mobile the menuList will high onclick by using below function called toggleMenu()
 var menuList = document.getElementById("menuList");
+
+//this will add zero pixel maximum height
         menuList.style.maxHeight = "0px"
 
+        /* in this function I will firstly check if the maximum 
+        height is zero, it will add 130 pixels otherwise maximum height is zero */
         function toggleMenu() {
             if (menuList.style.maxHeight == "0px") {
                 menuList.style.maxHeight = "130px"
